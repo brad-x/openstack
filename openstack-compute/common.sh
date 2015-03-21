3048aaddcf83c4214d6be5201d4a4563235b6cfb
Brad Laue <blaue@oculusinfo.com>
2015-03-21 08:09:16 -0400

    Bring openstack-compute script up to date with openstack-config method

---
+++ openstack-compute/common.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Configuration Setup
+source ./config
+
+# Install NTP
+yum -y install ntp cronie
+systemctl enable ntpd.service
+systemctl start ntpd.service
+systemctl enable crond.service
+systemctl start crond.service 
+
+# Set up OpenStack repos
+yum -y install yum-plugin-priorities epel-release
+yum -y install http://rdo.fedorapeople.org/openstack-juno/rdo-release-juno.rpm
+yum -y install openstack-utils
+yum -y update
+
+# Drop firewall and SELinux for now 
+systemctl stop firewalld.service
+systemctl disable firewalld.service
+sed -i 's/enforcing/disabled/g' /etc/selinux/config
+setenforce 0
+
+# Disable IPv6 for now 
+echo net.ipv6.conf.default.disable_ipv6=1 > /etc/sysctl.d/disable-ipv6.conf
+
+sysctl -p
+
+mkdir -pv /etc/openstack-uncharted/
+
+touch /etc/openstack-uncharted/common-setup-done
--- openstack-compute/compute-node.sh
+++ openstack-compute/compute-node.sh
@@ -1,77 +1,69 @@
 #!/bin/bash
 
+source ./config
+
 # Networking adjustments
 
-echo 'net.ipv4.conf.all.rp_filter=0' >> /etc/sysctl.conf
-echo 'net.ipv4.conf.default.rp_filter=0' >> /etc/sysctl.conf
-sysctl -p
+echo 'net.ipv4.conf.all.rp_filter=0' >> /etc/sysctl.d/rp-filter.conf
+echo 'net.ipv4.conf.default.rp_filter=0' >> /etc/sysctl.d/rp-filter.conf
 
-#get primary NIC info
-for i in $(ls /sys/class/net); do
-    if [ "$(cat /sys/class/net/$i/ifindex)" == '3' ]; then
-        NIC=$i
-        MY_MAC=$(cat /sys/class/net/$i/address)
-        echo "$i ($MY_MAC)"
-    fi
-done
+echo net.ipv6.conf.default.disable_ipv6=1 > /etc/sysctl.d/disable-ipv6.conf
+
+sysctl -p
 
 #nova compute
 yum -y install openstack-nova-compute sysfsutils libvirt-daemon-config-nwfilter
 
-sed -i.bak "/\[DEFAULT\]/a \
-rpc_backend = rabbit\n\
-rabbit_host = $CONTROLLER_IP\n\
-auth_strategy = keystone\n\
-my_ip = $THISHOST_IP\n\
-vnc_enabled = True\n\
-vncserver_listen = 0.0.0.0\n\
-vncserver_proxyclient_address = $THISHOST_IP\n\
-novncproxy_base_url = http://$CONTROLLER_IP:6080/vnc_auto.html\n\
-network_api_class = nova.network.neutronv2.api.API\n\
-security_group_api = neutron\n\
-linuxnet_interface_driver = nova.network.linux_net.LinuxOVSInterfaceDriver\n\
-firewall_driver = nova.virt.firewall.NoopFirewallDriver" /etc/nova/nova.conf
+openstack-config --set /etc/nova/nova.conf DEFAULT rpc_backend rabbit
+openstack-config --set /etc/nova/nova.conf DEFAULT rabbit_host $CONTROLLER_IP
+openstack-config --set /etc/nova/nova.conf DEFAULT auth_strategy keystone 
+openstack-config --set /etc/nova/nova.conf DEFAULT my_ip $THISHOST_IP
+openstack-config --set /etc/nova/nova.conf DEFAULT vnc_enabled True
+openstack-config --set /etc/nova/nova.conf DEFAULT vncserver_listen 0.0.0.0
+openstack-config --set /etc/nova/nova.conf DEFAULT vncserver_proxyclient_address $THISHOST_IP
+openstack-config --set /etc/nova/nova.conf DEFAULT novncproxy_base_url http://$CONTROLLER_IP:6080/vnc_auto.html
+openstack-config --set /etc/nova/nova.conf DEFAULT network_api_class nova.network.neutronv2.api.API
+openstack-config --set /etc/nova/nova.conf DEFAULT security_group_api neutron
+openstack-config --set /etc/nova/nova.conf DEFAULT linuxnet_interface_driver nova.network.linux_net.LinuxOVSInterfaceDriver
+openstack-config --set /etc/nova/nova.conf DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
 
-sed -i "/\[keystone_authtoken\]/a \
-auth_uri = http://$CONTROLLER_IP:5000/v2.0\n\
-identity_uri = http://$CONTROLLER_IP:35357\n\
-admin_tenant_name = service\n\
-admin_user = nova\n\
-admin_password = $SERVICE_PWD" /etc/nova/nova.conf
+openstack-config --set /etc/nova/nova.conf keystone_authtoken auth_uri http://$CONTROLLER_IP:5000/v2.0
+openstack-config --set /etc/nova/nova.conf keystone_authtoken identity_uri http://$CONTROLLER_IP:35357
+openstack-config --set /etc/nova/nova.conf keystone_authtoken admin_tenant_name service
+openstack-config --set /etc/nova/nova.conf keystone_authtoken admin_user nova
+openstack-config --set /etc/nova/nova.conf keystone_authtoken admin_password $SERVICE_PWD
 
-sed -i "/\[glance\]/a host = $CONTROLLER_IP" /etc/nova/nova.conf
+openstack-config --set /etc/nova/nova.conf glance host $CONTROLLER_IP
 
 #if compute node is virtual - change virt_type to qemu
 if [ $(egrep -c '(vmx|svm)' /proc/cpuinfo) == "0" ]; then
-    sed -i '/\[libvirt\]/a virt_type = qemu' /etc/nova/nova.conf
+    openstack-config --set /etc/nova/nova.conf libvirt virt_type qemu
 fi
 
 #install neutron
 yum -y install openstack-neutron-ml2 openstack-neutron-openvswitch
 
-sed -i '0,/\[DEFAULT\]/s//\[DEFAULT\]\
-rpc_backend = rabbit\n\
-rabbit_host = '"$CONTROLLER_IP"'\
-auth_strategy = keystone\
-core_plugin = ml2\
-service_plugins = router\
-allow_overlapping_ips = True/' /etc/neutron/neutron.conf
+openstack-config --set /etc/neutron/neutron.conf DEFAULT rpc_backend rabbit
+openstack-config --set /etc/neutron/neutron.conf DEFAULT rabbit_host $CONTROLLER_IP
+openstack-config --set /etc/neutron/neutron.conf DEFAULT auth_strategy keystone
+openstack-config --set /etc/neutron/neutron.conf DEFAULT core_plugin ml2
+openstack-config --set /etc/neutron/neutron.conf DEFAULT service_plugins router
 
-sed -i "/\[keystone_authtoken\]/a \
-auth_uri = http://$CONTROLLER_IP:5000/v2.0\n\
-identity_uri = http://$CONTROLLER_IP:35357\n\
-admin_tenant_name = service\n\
-admin_user = neutron\n\
-admin_password = $SERVICE_PWD" /etc/neutron/neutron.conf
+openstack-config --set /etc/neutron/neutron.conf keystone_authtoken auth_uri http://$CONTROLLER_IP:5000/v2.0
+openstack-config --set /etc/neutron/neutron.conf keystone_authtoken identity_uri http://$CONTROLLER_IP:35357
+openstack-config --set /etc/neutron/neutron.conf keystone_authtoken admin_tenant_name service
+openstack-config --set /etc/neutron/neutron.conf keystone_authtoken admin_user neutron
+openstack-config --set /etc/neutron/neutron.conf keystone_authtoken admin_password $SERVICE_PWD
 
-#edit /etc/neutron/plugins/ml2/ml2_conf.ini
-sed -i "/\[ml2\]/a \
-type_drivers = flat,vxlan\n\
-tenant_network_types = vxlan\n\
-mechanism_drivers = openvswitch" /etc/neutron/plugins/ml2/ml2_conf.ini
+openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 type_drivers flat,gre
+openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 tenant_network_types gre
+#openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 type_drivers flat,vxlan
+#openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 tenant_network_types vxlan
+openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 mechanism_drivers openvswitch
 
-openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_vxlan vni_ranges 1001:2000
-openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_vxlan vxlan_group 224.0.0.1
+openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_gre tunnel_id_ranges 1001:2000
+#openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_vxlan vni_ranges 1001:2000
+#openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_vxlan vxlan_group 224.0.0.1
 
 openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup enable_security_group True
 openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup enable_ipset True
@@ -79,22 +71,21 @@ openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup firew
 
 openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ovs local_ip $THISHOST_TUNNEL_IP
 openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ovs enable_tunneling True
-openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ovs bridge_mappings external:br-ex
 
-openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini agent tunnel_types vxlan
+openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini agent tunnel_types gre
+#openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini agent tunnel_types vxlan
 
 systemctl enable openvswitch.service
 systemctl start openvswitch.service
 
-sed -i "/\[neutron\]/a \
-url = http://$CONTROLLER_IP:9696\n\
-auth_strategy = keystone\n\
-admin_auth_url = http://$CONTROLLER_IP:35357/v2.0\n\
-admin_tenant_name = service\n\
-admin_username = neutron\n\
-admin_password = $SERVICE_PWD" /etc/nova/nova.conf
+openstack-config --set /etc/nova/nova.conf neutron url http://$CONTROLLER_IP:9696
+openstack-config --set /etc/nova/nova.conf neutron auth_strategy keystone
+openstack-config --set /etc/nova/nova.conf neutron admin_auth_url http://$CONTROLLER_IP:35357/v2.0
+openstack-config --set /etc/nova/nova.conf neutron admin_tenant_name service
+openstack-config --set /etc/nova/nova.conf neutron admin_username neutron
+openstack-config --set /etc/nova/nova.conf neutron admin_password $SERVICE_PWD
 
-ln -s /etc/neutron/plugins/ml2/ml2_conf.ini /etc/neutron/plugin.ini
+ln -sf /etc/neutron/plugins/ml2/ml2_conf.ini /etc/neutron/plugin.ini
 
 cp /usr/lib/systemd/system/neutron-openvswitch-agent.service \
   /usr/lib/systemd/system/neutron-openvswitch-agent.service.orig
@@ -108,27 +99,31 @@ systemctl enable neutron-openvswitch-agent.service
 systemctl start neutron-openvswitch-agent.service
 
 #cinder storage node
-pvcreate /dev/sdb
-vgcreate cinder-volumes /dev/sdb
+#pvcreate /dev/sdb
+#vgcreate cinder-volumes /dev/sdb
 
 yum -y install openstack-cinder targetcli python-oslo-db MySQL-python
 
-sed -i.bak "/\[database\]/a connection = mysql://cinder:$SERVICE_PWD@$CONTROLLER_IP/cinder" /etc/cinder/cinder.conf
-sed -i '0,/\[DEFAULT\]/s//\[DEFAULT\]\
-rpc_backend = rabbit\
-rabbit_host = '"$CONTROLLER_IP"'\
-auth_strategy = keystone\
-my_ip = '"$THISHOST_IP"'\
-iscsi_helper = lioadm/' /etc/cinder/cinder.conf
-sed -i "/\[keystone_authtoken\]/a \
-auth_uri = http://$CONTROLLER_IP:5000/v2.0\n\
-identity_uri = http://$CONTROLLER_IP:35357\n\
-admin_tenant_name = service\n\
-admin_user = cinder\n\
-admin_password = $SERVICE_PWD" /etc/cinder/cinder.conf
+echo "192.168.0.6:/export/openstack" > /etc/cinder/nfsshares
 
-systemctl enable openstack-cinder-volume.service target.service
-systemctl start openstack-cinder-volume.service target.service
+openstack-config --set /etc/cinder/cinder.conf database connection mysql://cinder:$SERVICE_PWD@$CONTROLLER_IP/cinder
+
+openstack-config --set /etc/cinder/cinder.conf DEFAULT rpc_backend rabbit
+openstack-config --set /etc/cinder/cinder.conf DEFAULT rabbit_host $CONTROLLER_IP
+openstack-config --set /etc/cinder/cinder.conf DEFAULT auth_strategy keystone
+openstack-config --set /etc/cinder/cinder.conf DEFAULT my_ip $THISHOST_IP
+#openstack-config --set /etc/cinder/cinder.conf DEFAULT iscsi_helper lioadm
+openstack-config --set /etc/cinder/cinder.conf DEFAULT volume_driver cinder.volume.drivers.nfs.NfsDriver
+openstack-config --set /etc/cinder/cinder.conf DEFAULT nfs_shares_config /etc/cinder/nfsshares
+
+openstack-config --set /etc/cinder/cinder.conf keystone_authtoken auth_uri http://$CONTROLLER_IP:5000/v2.0
+openstack-config --set /etc/cinder/cinder.conf keystone_authtoken identity_uri http://$CONTROLLER_IP:35357
+openstack-config --set /etc/cinder/cinder.conf keystone_authtoken admin_tenant_name service
+openstack-config --set /etc/cinder/cinder.conf keystone_authtoken admin_user cinder
+openstack-config --set /etc/cinder/cinder.conf keystone_authtoken admin_password $SERVICE_PWD
+
+systemctl enable openstack-cinder-volume.service # target.service
+systemctl start openstack-cinder-volume.service # target.service
 
 echo 'export OS_TENANT_NAME=admin' > creds
 echo 'export OS_USERNAME=admin' >> creds
